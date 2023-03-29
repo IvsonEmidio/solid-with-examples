c93832943de379dd6bd3dcd03e3f94b5414f81fa
Ivson Emidio <ivson.silva@dpsp.com.br>
2023-03-29 17:49:28 -0300

    refactor(srp): business logic and car-repository

--- SRP/oop/wrong/user.controller.ts
+++ SRP/oop/wrong/user.controller.ts
@@ -1,11 +1,14 @@
 import UserRepository from "../mock/user.repository";
+import CarRepository from "../mock/car.repository";
 
 //Exemplo de controller "Deus"
 export default class UserController {
     private userRepository: UserRepository;
+    private carRepository: CarRepository;
 
     constructor() {
         this.userRepository = new UserRepository();
+        this.carRepository = new CarRepository();
     }
 
     public async getUser(userId: number) {
@@ -14,10 +17,19 @@ export default class UserController {
         }
 
         const userData = await this.userRepository.findOne(userId);
-        const carData = await this.userRepository.findOne(userId)
+        const carData = await this.carRepository.findOne(userId)
 
         if (!userData || !carData) {
             throw new Error('Not found');
+        }
+
+        // Lógica de négocio
+        if (userData.age > 18) {
+            userData.fullname = `${userData.fullname}, MAIOR DE IDADE`;
+        }
+
+        if (carData.year < 2010) {
+            carData.depreciation = 'HIGH';
         }
 
         return {
